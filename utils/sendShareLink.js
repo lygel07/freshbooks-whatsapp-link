ce6c7c33813e970551e74d4223c30a1da05f18fa
“lygel07” <lygel.07@gmail.com>
2022-04-22 14:24:20 +0200

    rename function

--- utils/sendShareLink.js
+++ utils/sendShareLink.js
@@ -13,12 +13,10 @@ module.exports = async (accountId,invoiceId)=>{
         const app = new Client(clientId,token);
         const share_link = await app.invoices.shareLink(accountId,invoiceId);
         const invoiceInfo = await app.invoices.single(accountId,invoiceId);
-        const clinetInfo  = await app.clients.single(accountId,invoiceInfo.data.customerId);   
-//        console.log(invoiceInfo.data.customerId);
-//        console.log(clinetInfo.data.mobPhone);
-//        console.log(share_link.data)
+        const client  = await app.clients.single(accountId,invoiceInfo.data.customerId);   
 
-        postwhatsapp(share_link.data.shareLink, clinetInfo.data.mobPhone);
+
+        postwhatsapp(share_link.data.shareLink, client.data.mobPhone);
       
       } catch (error) {
       console.log(error);  
